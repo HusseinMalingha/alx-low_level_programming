7640e2775bdedf3d95ac05f8488e7e4c1eaaf82e
HusseinMalingha <husseinmalingha@gmail.com>
2023-02-28 00:43:12 -0800

    Removed the main funtion from 0-reset_to_98 function

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,12 +3,12 @@
 
 /**
  * reset_to_98 - updates the value it points to to 98
- *
- * inputs: int *n
+ *@n: the pointer storing  address to reset
  *
  * Return: None
  */
 void reset_to_98(int *n)
 {
+	/* *n is a pointer that points to the addres input in the function */
 	*n = 98;
 }
