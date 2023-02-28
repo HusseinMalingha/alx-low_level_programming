957d69b053124b37ab1617a67d748962f83ef6b7
HusseinMalingha <husseinmalingha@gmail.com>
2023-02-28 00:35:45 -0800

    Removed the main funtion from 0-reset_to_98 function

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,24 +2,6 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * Return: Always 0 (Success)
- */
-int main()
-{
-	/* declaratio of n an integer whose value will be changed */
-	int n;
-
-	/* n is initialized with 402 */
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
-}
-
-/**
  * reset_to_98 - updates the value it points to to 98
  *
  * inputs: int *n
