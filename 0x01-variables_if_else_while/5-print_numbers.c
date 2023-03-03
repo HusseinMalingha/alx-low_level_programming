71c1d5ce231079e96f5fd5d627e44041c0c4aa48
HusseinMalingha <husseinmalingha@gmail.com>
2023-03-03 08:32:06 -0800

    Added a c program to print all sigle digit base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers in base 10
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int single_dig;
+
+	for (single_dig = 0; single_dig < 10; single_dig++)
+	{
+		printf("%d\n", single_dig);
+	}
+	return (0);
+}
