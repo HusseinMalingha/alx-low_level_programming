9d759fe6bf692b56d29eb49c4d7770c0d6d61224
HusseinMalingha <husseinmalingha@gmail.com>
2023-03-03 03:15:10 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXWYZ";
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i = 0;
-	while(i < 52)
+	while (i < 52)
 	{
 		putchar(alphabet[i]);
 		i++;
