6f98ec88d300fe0e2a376af115380a28edf9b869
HusseinMalingha <husseinmalingha@gmail.com>
2023-11-06 11:50:52 -0800

    Fixed linting errors

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,13 +13,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	int name_length, owner_length;
 
-	// Allocate memory for the dog structure
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (name == NULL || owner == NULL)
-		return NULL;
+		return (NULL);
 
-	// Calculate the length of the name and owner strings
 	name_length = 0;
 	owner_length = 0;
 	while (name[name_length])
@@ -28,25 +26,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 		owner_length++;
 
 	if (new_dog == NULL)
-		return NULL;
+		return (NULL);
 
-	// Allocate memory for the name and owner strings
-	new_dog->name = malloc(name_length + 1);   // +1 for the null terminator
+	new_dog->name = malloc(name_length + 1);
 	new_dog->owner = malloc(owner_length + 1);
 
-	if (new_dog->name == NULL || new_dog->owner == NULL) {
+	if (new_dog->name == NULL || new_dog->owner == NULL)
+	{
 		free(new_dog->name);
 		free(new_dog->owner);
 		free(new_dog);
-		return NULL;
+		return (NULL);
 	}
 
-	// Copy the name and owner strings
 	for (int i = 0; i <= name_length; i++)
 		new_dog->name[i] = name[i];
 	for (int i = 0; i <= owner_length; i++)
 		new_dog->owner[i] = owner[i];
 
 	new_dog->age = age;
-	return new_dog;
+	return (new_dog);
 }
