ba62031d23b1bd817f50b1413cb367b09e7723ec
HusseinMalingha <husseinmalingha@gmail.com>
2023-11-06 11:45:14 -0800

    Im pla

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,11 +8,12 @@
  * @owner: dog owner
  */
 struct dog {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
+// typedef
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
