64a0538723fce4deb7f973804b14d0388d0dd313
HusseinMalingha <husseinmalingha@gmail.com>
2023-11-20 23:13:43 -0800

    First Commit to recursion. I think.!

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a recursive function that prints caracters to the console
+ * @s: a string of characters
+ *
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+		return;
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
