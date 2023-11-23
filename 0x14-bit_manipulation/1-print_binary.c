48cc5501bf186dee1d985024179a3dd3074da45d
HusseinMalingha <husseinmalingha@gmail.com>
2023-11-23 10:52:29 -0800

    Removed trailing zeros, and unnecesary newlines

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,14 +7,22 @@
 void print_binary(unsigned long int n)
 {
 	int position = sizeof(unsigned long int) * 8 - 1;
+	int started = 0;
 
 	while (position >= 0)
 	{
 		unsigned long int mask = 1UL << position;
+		int bit = ((n & mask) ? 1 : 0);
 
-		_putchar((n & mask) ? '1' : '0');
+		if (bit || started)
+		{
+			_putchar(bit ? '1' : '0');
+			started = 1;
+		}
+
 		position--;
 	}
 
-	_putchar('\n');
+	if (!started)
+		_putchar('0');
 }
